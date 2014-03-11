6e1b29312c1c76bee746aa6fd959e3eaf73e04f5
Aude <aude@labs-vmbuilder-precise.pmtpa.wmflabs>
2014-03-11 08:06:57 +0000

    fix spaces

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,7 +3,7 @@ class wikidata_test() {
     generic::systemuser { 'mwdeploy': name => 'mwdeploy' }
 
     class {'wikidata_test::testhome':}
-	class {'wikidata_test::static':}
+    class {'wikidata_test::static':}
     class {'wikidata_test::config':}
     class {'wikidata_test::mediawiki':}
     class {'wikidata_test::mwextensions':}
